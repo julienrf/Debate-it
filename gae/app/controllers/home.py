160333c15f42c1e0088960cf213aa222e06b181a
Engleek <engleek@engleek.com>
2010-07-18 21:30:41 +0200

    Added basic app structure. Two controllers, two views.

---
+++ gae/app/controllers/home.py
@@ -0,0 +1,11 @@
+import cgi
+import os
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+
+class Home(webapp.RequestHandler):
+	def get(self):
+		vars = {}
+		path = os.path.join(os.path.dirname(__file__), '../views/home.html')
+		self.response.out.write(template.render(path, vars))
