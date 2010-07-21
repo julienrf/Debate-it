93bdd193c57e1de1db5e72a18e5fcb8baf39edd7
Engleek <engleek@engleek.com>
2010-07-21 22:05:28 +0200

    Added rudimentary i18n system.

--- gae/app/controllers/home.py
+++ gae/app/controllers/home.py
@@ -4,8 +4,13 @@ import os
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 
+from app.i18n.eng import lang
+
 class Home(webapp.RequestHandler):
 	def get(self):
-		vars = {}
+		vars = {
+		  "lang": lang
+		}
+		
 		path = os.path.join(os.path.dirname(__file__), '../views/home.html')
 		self.response.out.write(template.render(path, vars))
