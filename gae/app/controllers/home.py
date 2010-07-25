221b947451da6cde087bbec426448b51f4773adc
Engleek <redeye4@gmail.com>
2010-07-25 16:58:15 +0200

    Added basic language selection based on Accept-Language HTTP header.

--- gae/app/controllers/home.py
+++ gae/app/controllers/home.py
@@ -4,13 +4,22 @@ import os
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 
-from app.i18n.eng import lang
+import yaml
+import markdown
 
 class Home(webapp.RequestHandler):
 	def get(self):
+		langheader = self.request.headers['Accept-Language']
+        
+		if langheader.startswith('fr'):
+			langfile = file(os.path.join(os.path.dirname(__file__), '../i18n/fr/home.yaml'))
+		else:
+			langfile = file(os.path.join(os.path.dirname(__file__), '../i18n/en/home.yaml'))
+
 		vars = {
-		  "lang": lang
+		  "lang": yaml.load(langfile)
 		}
+		langfile.close()
 		
 		path = os.path.join(os.path.dirname(__file__), '../views/home.html')
 		self.response.out.write(template.render(path, vars))
